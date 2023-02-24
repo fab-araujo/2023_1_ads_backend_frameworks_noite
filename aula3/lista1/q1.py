93b26d983bb829733d57c3630a7cbcb80ddfa034
fabricioaraujo <faa@ufpa.br>
2023-02-24 19:00:47 -0300

    Create q1.py

---
+++ aula3/lista1/q1.py
@@ -0,0 +1,9 @@
+usuario = input("Digite o seu usuário: ")
+senha = input("Digite a sua senha ")
+
+while(usuario == senha):
+    print("Usuário e senha não podem ser iguais! Tente novamente!")
+    usuario = input("Digite o seu usuário: ")
+    senha = input("Digite a sua senha ")
+
+print("Usuário e senha são válidos. Obrigado!")
