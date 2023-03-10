dfe711782be77e824b55c20b1673f2a9d6fa46d8
fabricioaraujo <faa@ufpa.br>
2023-03-10 14:59:48 -0300

    Aula 4

--- aula4/projeto/mysite/polls/urls.py
+++ aula4/projeto/mysite/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail/', views.detail, name='detail'),
-    path('contact/', views.contact, name='contact')
+    path('', views.index, name="index"),
+    path('detail/', views.detail, name="detail"),
+    path('contact/', views.contact, name="contact")
 ]
--- aula4/projeto/mysite/polls/views.py
+++ aula4/projeto/mysite/polls/views.py
@@ -3,12 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Olá, mundo!")
 
 def detail(request):
-    return HttpResponse("Pagina de detail")
+    return HttpResponse("Página detail!")
 
 def contact(request):
-    return HttpResponse("Pagina de contact")
+    return HttpResponse("Página contact!")
